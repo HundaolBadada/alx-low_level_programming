6a1ddcc11bfdf40c749fa4391bdcae9ebe95980b
Hundaol Badada <hundaolbadho@gmail.com>
2024-01-08 11:53:15 -0800

    100-set_string.c

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,1 +1,14 @@
-abc123
+/**
+ * set_string - sets the value of a pointer to a char
+ *
+ * @s: pointer to pointer to a char
+ *
+ * @to: the address to match
+ *
+ * Return: void
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
