7c7d5e02f07e5f76b4ed1a291358aea00a2d5d40
Hundaol Badada <hundaolbadho@gmail.com>
2024-02-01 05:28:27 -0800

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,17 +1,12 @@
 #include <stdio.h>
 
-#include "lists.h"
-
 /**
- * before_main - funcion that prints a specific string before the main,
- * function in executed.
+ * main - check the code
  *
- * Return: no return.
+ * Return: Always 0.
  */
-
-
-void before_main(void)
+int main(void)
 {
-	printf("You're beat! and yet, you must allow,\n");
-	printf("I bore my house upon my back!\n");
+    printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
+    return (0);
 }
